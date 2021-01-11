1dfdfd52d43f0c7bacbbbde83a91dc7fefff0504
Hope <nadezhda.barinova@gmail.com>
2021-01-11 16:28:53 +0200

    init comit start dnd api

---
+++ DnDBackend/DnDBackend/Models/Character.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DnDBackend.Models
+{
+    public class Character
+    {
+        public long Id { get; set; }
+        public string Race { get; set;  }
+        public string FightClass { get; set; }
+        public object Abilities { get; set; }
+        public object Description { get; set; }
+        public object Equipment { get; set; }
+    }
+}
